9e40ca0018ff2d004b2018db6f9c6a44e0a504f0
Taz khan <55050503+tazkhan24@users.noreply.github.com>
2022-03-28 19:31:39 +0530

    Added solution - LeetHub

---
+++ Middle of Three - GFG/middle-of-three.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int middle(int a, int b, int c){
+        
+        if(a>b && a<c || a>c && a<b)
+            return a;
+        if(b>a && b<c || b>c && b<a)
+            return b;
+        return c;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
