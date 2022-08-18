0a31c369f4e4bb9d86182a2b5218c5378a9b18cb
Taz khan <55050503+tazkhan24@users.noreply.github.com>
2022-08-18 23:51:53 +0530

    Time: 206 ms (94.14%), Space: 78.3 MB (79.94%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int> cnt;
+        for(int x: arr)
+            ++cnt[x];
+        vector<int> frequencies;
+        for(auto [_,freq]:cnt)
+            frequencies.push_back(freq);
+        sort(frequencies.begin(),frequencies.end());
+        
+        int ans =0, removed=0,i = frequencies.size()-1, half = arr.size()/2;
+        while(half>removed){
+            ans+=1;
+            removed+=frequencies[i--];
+        }
+        return ans;
+        
+    }
+};
