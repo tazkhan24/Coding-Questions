6569f5e93c0986721ed7f685e167a253c0d3f2ab
Taz khan <55050503+tazkhan24@users.noreply.github.com>
2022-08-25 23:34:41 +0530

    Time: 102 ms (61.90%), Space: 48.2 MB (93.51%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reachable = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(i>reachable)
+                return false;
+            reachable = max(reachable, nums[i]+i);
+        }
+        return true;
+    }
+};
