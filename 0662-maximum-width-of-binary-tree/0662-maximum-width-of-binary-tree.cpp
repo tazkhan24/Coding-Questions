8e4b6fba18d6562c6f4d86fafdcb3cbb19f53a89
Taz khan <55050503+tazkhan24@users.noreply.github.com>
2023-01-22 13:10:29 +0530

    Time: 11 ms (66.69%), Space: 17.4 MB (28.35%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -21,7 +21,7 @@ public:
             int first,last;
             int mmin = q.front().second;
             for(int i=0;i<size;i++){
-                int cur_id = q.front().second;
+                int cur_id = q.front().second-mmin;
                 TreeNode* node = q.front().first;
                 q.pop();
                 if(i==0) first = cur_id;
