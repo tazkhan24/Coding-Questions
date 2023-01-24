31e27857a564f26aabb09f974fc8e1f2b1dc08db
Taz khan <55050503+tazkhan24@users.noreply.github.com>
2023-01-24 11:24:13 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.55%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
+    int dp[20]{};
     int numTrees(int n) {
        if(n<=1) return 1;
-        int ans=0;
+        if(dp[n]) return dp[n];
         for(int i=1;i<=n;i++){
-            ans+=numTrees(i-1)*numTrees(n-i);
+            dp[n]+=numTrees(i-1)*numTrees(n-i);
         }
-        return ans;
+        return dp[n];
     }
 };
