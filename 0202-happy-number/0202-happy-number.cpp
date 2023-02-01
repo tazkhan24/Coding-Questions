6a5022743c1521b17d744ea81b899892d94bf3fc
Taz khan <55050503+tazkhan24@users.noreply.github.com>
2023-02-01 11:45:18 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (26.77%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+       unordered_set<int> tmp;
+        while(n!=1){
+            if(tmp.find(n) == tmp.end()){
+                tmp.insert(n);
+            }
+            else {
+                return false;
+            }
+            int sum=0;
+            while(n!=0) {
+                int rem = n%10;
+                sum+=rem*rem;
+                n/=10;
+                }
+            n = sum;
+            
+        }
+        return true;
+    }
+};
