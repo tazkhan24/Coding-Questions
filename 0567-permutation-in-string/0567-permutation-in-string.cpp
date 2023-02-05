0b1d04a5b9ec51eefec3a4c21648b40c607b3a8e
Taz khan <55050503+tazkhan24@users.noreply.github.com>
2023-02-05 12:20:16 +0530

    Time: 45 ms (22.21%), Space: 29.9 MB (11.78%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    bool isequal(vector<int> p, vector<int> q){
+        for(int i=0;i<p.size();i++){
+            if(p[i]!=q[i]) return false;
+        }
+        return true;
+    }
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s2.size()<s1.size()) return false;
+        vector<int> freqS1(26,0),freqS2(26,0);
+        for(char s: s1) freqS1[s-'a']++;
+        int i=0,j=0;
+        while(j<s2.size()){
+            freqS2[s2[j]-'a']++;
+            if(j-i+1 == s1.size()){
+                if(isequal(freqS1,freqS2)==true) return true;
+            }
+            if(j-i+1<s1.size()) j++;
+            else
+            {
+                freqS2[s2[i]-'a']--;
+                i++;
+                j++;
+            }
+        }
+        return false;
+    }
+};
